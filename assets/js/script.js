a389cb2ef6c1354273d7ad3003de44a26a8cdf46
Mouse0226 <mouse1987.ml@gmail.com>
2022-02-02 20:10:24 -0600

    Added exchange rate API call

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+var getExchangeRate = function(country) {
+    var apiUrl = "https://v6.exchangerate-api.com/v6/5b54235bc02a7caa763ae076/pair/USD/" + country;
+
+    fetch (apiUrl)
+        .then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                    returnRate(data);
+                });
+            } else {
+                handleError();
+            }
+        })
+        .catch(function(error) {
+            handleError();
+        });
+};
+
+var returnRate = function(data) {
+    // Code to display rate to user
+};
+
+var handleError = function() {
+    // Do something to handle the error without using prompts/alerts
+};
