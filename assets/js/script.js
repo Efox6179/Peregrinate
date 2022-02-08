e5124a7d0b38a1f3a612f2cd1c0fe83491abfc86
Efox6179 <95189308+Efox6179@users.noreply.github.com>
2022-02-07 20:37:43 -0600

    updated html

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+var getExchangeRate = function(country) {
+    var apiUrl = "https://v6.exchangerate-api.com/v6/5b54235bc02a7caa763ae076/pair/USD/" + country;
+
+    fetch (apiUrl)
+        .then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                    returnRate(data);
+                });
+            } else {
+                handleError();
+            }
+        })
+        .catch(function(error) {
+            handleError();
+        });
+};
+
+var returnRate = function(data) {
+    // Code to display rate to user
+};
+
+var handleError = function() {
+    // Do something to handle the error without using prompts/alerts
+};
+
+$(".drop-menu").on("click","a",function() {
+   var currency = $(this).attr("currency-code");
+   getExchangeRate(currency); 
+   var flagCode = $(this).attr("flag-code");
+   getFlag(flagCode/*, country name for the alt attribute*/);
+});
+
+function getFlag(flagCode) {
+   $(".flag-img").attr("src",`https://flagcdn.com/${flagCode}.svg`);
+   //$(".flag-img").attr("alt",countryName);
+}
