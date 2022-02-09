b2a7f4b249956fa12cfd7c2de6f80d48df46365f
Quntarious <90361495+Quntarious@users.noreply.github.com>
2022-02-08 18:15:35 -0600

    button dropdown bulma

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,33 @@
+const dropdowns = document.querySelectorAll('.dropdown:not(.is-hoverable)');
+
+if (dropdowns.length > 0) {
+  dropdowns.forEach(function(el) {
+    el.addEventListener('click', function(e) {
+    closeDropdowns();
+      e.stopPropagation();
+      el.classList.toggle('is-active');
+    });
+  });
+
+  document.addEventListener('click', function(e) {
+    closeDropdowns();
+  });
+}
+
+function closeDropdowns() {
+  dropdowns.forEach(function(el) {
+    el.classList.remove('is-active');
+  });
+}
+
+// Close dropdowns if ESC pressed
+document.addEventListener('keydown', function (event) {
+  let e = event || window.event;
+  if (e.key === 'Esc' || e.key === 'Escape') {
+    closeDropdowns();
+  }
+});
+
 var getExchangeRate = function(country) {
     var apiUrl = "https://v6.exchangerate-api.com/v6/5b54235bc02a7caa763ae076/pair/USD/" + country;
 
@@ -35,3 +65,11 @@ function getFlag(flagCode) {
    $(".flag-img").attr("src",`https://flagcdn.com/${flagCode}.svg`);
    //$(".flag-img").attr("alt",countryName);
 }
+
+var dropdown = document.querySelector('.dropdown');
+dropdown.addEventListener('click', function(event) {
+  event.stopPropagation();
+  dropdown.classList.toggle('is-active');
+});
+
+
